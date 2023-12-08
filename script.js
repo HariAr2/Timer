aa2baea720c8f3e85104b4ae3df0b506e246af63
Hari Aravindh <127916422+HariAr2@users.noreply.github.com>
2023-12-08 18:46:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const launchDate = '4 Apr 2025';
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+function countdown() {
+    const currentDate = new Date();
+    const formattedLaunchDate = new Date(launchDate);
+
+    const timeDifference = formattedLaunchDate - currentDate;
+    const totalSeconds = Math.floor(timeDifference / 1000); 
+
+    const daysDifference = Math.floor(totalSeconds / (24 * 60 * 60));
+    const hoursDifference = Math.floor((totalSeconds % (24 * 60 * 60)) / (60 * 60));
+    const minutesDifference = Math.floor((totalSeconds % (60 * 60)) / 60);
+    const secondsDifference = Math.floor(totalSeconds % 60);
+
+    daysEl.innerHTML = formatTime(daysDifference);
+    hoursEl.innerHTML = formatTime(hoursDifference);
+    minutesEl.innerHTML = formatTime(minutesDifference);
+    secondsEl.innerHTML = formatTime(secondsDifference);
+}
+function formatTime(time){
+    return time < 10 ? `0${time}` : time;
+}
+
+countdown(); 
+setInterval(countdown, 1000); 
