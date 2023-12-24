01941d0551f0d0e91ae1293bbe886e880fdc5eba
HariAr2 <ha7014@srmist.edu.in>
2023-12-24 22:10:28 +0530

    added comments

--- script.js
+++ script.js
@@ -5,21 +5,21 @@ const minutesEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
 
 function countdown() {
-    const currentDate = new Date(); 
-    const formattedLaunchDate = new Date(launchDate);
+    const currentDate = new Date(); // Get current date
+    const formattedLaunchDate = new Date(launchDate); // Convert launch date to Date object
 
-    const timeDifference = formattedLaunchDate - currentDate;
-    const totalSeconds = Math.floor(timeDifference / 1000); 
+    const timeDifference = formattedLaunchDate - currentDate; // Calculate time difference in milliseconds
+    const totalSeconds = Math.floor(timeDifference / 1000); // Convert to seconds
 
-    const daysDifference = Math.floor(totalSeconds / (24 * 60 * 60));
-    const hoursDifference = Math.floor((totalSeconds % (24 * 60 * 60)) / (60 * 60));
-    const minutesDifference = Math.floor((totalSeconds % (60 * 60)) / 60);
-    const secondsDifference = Math.floor(totalSeconds % 60);
+    const daysDifference = Math.floor(totalSeconds / (24 * 60 * 60)); // Calculate days difference
+    const hoursDifference = Math.floor((totalSeconds % (24 * 60 * 60)) / (60 * 60)); // Calculate hours difference
+    const minutesDifference = Math.floor((totalSeconds % (60 * 60)) / 60); // Calculate minutes difference
+    const secondsDifference = Math.floor(totalSeconds % 60); // Calculate seconds difference
 
-    daysEl.innerHTML = formatTime(daysDifference);
-    hoursEl.innerHTML = formatTime(hoursDifference);
-    minutesEl.innerHTML = formatTime(minutesDifference);
-    secondsEl.innerHTML = formatTime(secondsDifference);
+    daysEl.innerHTML = formatTime(daysDifference); // Display days difference
+    hoursEl.innerHTML = formatTime(hoursDifference); // Display hours difference
+    minutesEl.innerHTML = formatTime(minutesDifference); // Display minutes difference
+    secondsEl.innerHTML = formatTime(secondsDifference); // Display seconds difference
 }
 function formatTime(time){
     return time < 10 ? `0${time}` : time;
